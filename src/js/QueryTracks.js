46cc99c8024bfa9777cf472eaf0ae3e2c95f2b9e
Joe McConnell <joe.mcconnell10@gmail.com>
2022-06-16 22:45:21 -0500

    removed buttons but left texf

--- src/js/QueryTracks.js
+++ src/js/QueryTracks.js
@@ -10,16 +10,6 @@ export default function QueryTracks() {
   const [users , setUsers] = useState([]);
   const [activityVar, setActivityVar] = useState('');
   
-{/*
-  useEffect(()=>{
-    const fetchData = async() => {
-      const response = await axios.get("https://20gefk5dd7.execute-api.us-east-1.amazonaws.com/dev/query");
-      setUsers(response.data)
-    }
-    fetchData();
-  },[])
-*/}
-
   useEffect(()=>{
     apiGet();
   },[])
@@ -48,25 +38,9 @@ export default function QueryTracks() {
     <div>
       {users.map(item=>(
 
-
-            <MDBox mb={1.5}>
-                
-                <MDButton
-                    rel="noreferrer"
-                    variant="gradient"
-                    color="info"
-                    size="large"
-                    fullWidth 
-                    primary onClick={() => apiCall(item.activity,item.trackName)}
-                >
                     <div>
-                    
                         {item.activity}
-                        
                     </div>
-                </MDButton>
-
-            </MDBox>
       ))}
 
     </div>
